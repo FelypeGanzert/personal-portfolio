04ee1e6c861c434fd49af86fbc085f5ca42ab414
Felype Ganzert <39774570+FelypeGanzert@users.noreply.github.com>
2020-04-10 18:53:30 -0300

    added animations effects

---
+++ dist/app.js
@@ -0,0 +1,34 @@
+// Menu
+document.getElementById('menu').addEventListener('click', () => {
+    document.getElementById('menu').classList.toggle('show');
+});
+
+// Project entry animation
+document.addEventListener('DOMContentLoaded', addProjects);
+document.addEventListener('scroll', addProjects);
+
+function addProjects() {
+    const UIevents = document.querySelectorAll('.project');
+    UIevents.forEach(el => {
+        if (isVisible(el)) {
+            el.classList.add('box-enter');
+        } else{
+            el.classList.remove('box-enter');
+        }
+    });
+}
+
+function isVisible(element) {
+    let elPositionYstart = element.getBoundingClientRect().top + window.scrollY + 150;
+    let elPositionYend = element.getBoundingClientRect().top + element.offsetHeight + window.scrollY - 150;
+    let pageYstart = window.pageYOffset;
+    let pageYend = pageYstart + window.innerHeight;
+
+    if ((elPositionYstart >= pageYstart && elPositionYstart <= pageYend) ||
+        (elPositionYend >= pageYstart && elPositionYend <= pageYend) ||
+        (elPositionYstart <= pageYstart && elPositionYend >= pageYend)) {
+        return true;
+    } else {
+        return false;
+    }
+}
